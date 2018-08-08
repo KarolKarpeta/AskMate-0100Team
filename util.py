d0bd6dc1b1deac478003c26c6b8aad415e8e3b22
Grzegorz Stepniak <grzechu31@gmail.com>
2018-08-08 12:40:19 +0200

    times converts added

--- util.py
+++ util.py
@@ -1,5 +1,7 @@
+from datetime import datetime
 import base64
 import persistence
+import time
 
 
 def string_to_base64(s):
@@ -16,3 +18,17 @@ def get_headers_on_main_site(a):
     for row in a:
         new_dic__ist.append(dict((key, value) for key, value in row.items() if key in headers[:5]))
     return new_dic__ist
+
+def generate_time_in_UNIX():
+    my_list=[]
+    today = datetime.now().strftime("%Y-%m-%d %H:%M")
+    my_list.append(today)
+    unix = time.mktime(datetime.strptime(today, "%Y-%m-%d %H:%M").timetuple())
+
+    return int(unix)
+
+def convert_unix_to_time_str(unix):
+    norm_time= time.strftime("%Y-%m-%d %H:%M", time.localtime(int(unix)))
+
+    return norm_time
+
